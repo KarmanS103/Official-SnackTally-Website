21259944e435a0a8f7281aba11534a53452dc538
plyalyut <pavlo_lyalyutskyy@brown.edu>
2017-04-06 19:42:49 -0400

    Check Conditions

--- dist/connectdb.php
+++ dist/connectdb.php
@@ -14,16 +14,35 @@ else {
   echo "Connected To Database";
 }
 
+function filter($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+}
+
 $email = $_POST["Email"];
 $username = $_POST["USN"];
 $password = $_POST["Pass"];
 $passver = $_POST["PassVer"];
+
+$nameErr = $emailErr = $passErr = $websiteErr = "";
+if($password != $passver)
+{
+  $passErr = "Passwords do not match";
+}
+
+if(preg_match($username))
+{
+  $nameErr = "Username has";
+}
+
 
 if($password == $passver){
 $query = "INSERT INTO `SnackTally`.`User` (`Email`, `First Name`,
   `Last Name`, `Photo`, `Pass`, `Username`, `GroupID`)
   VALUES ('$email', 'test', 'test', NULL, '$password', '$username', 0)";
 }
+
 
 mysqli_query($con, $query);
 if (!$query) { die(mysqli_error($db_connect)); }
