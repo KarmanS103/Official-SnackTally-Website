0c09ee514fbf65261db157ca077e2b8f5f00e160
plyalyut <pavlo_lyalyutskyy@brown.edu>
2017-04-06 21:00:43 -0400

    Fixed bug

--- dist/connectdb.php
+++ dist/connectdb.php
@@ -41,7 +41,7 @@ if(preg_match($username)==1)
 }
 
 $select = mysqli_query($con, "SELECT `Email` FROM `SnackTally`.`User` WHERE `Email` = '$email'");
-if(mysqli_num_rows($result) > 0))
+if(mysqli_num_rows($result) > 0)
 {
     $emailErr = "Email already exists";
     $gate = False;
